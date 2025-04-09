53562bfad640bedb8d088c49bc9fc0a469ab6eae
Chasan <chasanalim07@gmail.com>
2025-04-09 10:41:41 +0700

    Banmod | Tambah fitur mata password dan ganti background

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,6 +1,5 @@
 import { Head, Link, useForm } from "@inertiajs/react";
 import React, { useState } from "react";
-
 import { Form, Button, Alert } from "react-bootstrap";
 
 export default function Login({ status, canResetPassword }) {
@@ -12,6 +11,10 @@ export default function Login({ status, canResetPassword }) {
 
     const [show, setShow] = useState(false);
     const [loading, setLoading] = useState(false);
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePassword = () => setShowPassword((prev) => !prev);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -26,7 +29,7 @@ export default function Login({ status, canResetPassword }) {
 
     return (
         <div
-            className="sign-in__wrapper"
+            className="sign-in__wrapper banner"
             // style={{ backgroundImage: `url(${BackgroundImage})` }}
         >
             <Head title="Login" />
@@ -44,30 +47,57 @@ export default function Login({ status, canResetPassword }) {
                     src={Logo}
                     alt="logo"
                 /> */}
-                <div className="h4 mb-2 text-center">Sign In</div>
+                <div className="h3 mb-3 text-center">Sign In</div>
                 <Form.Group className="mb-2" controlId="username">
                     <Form.Label>Email</Form.Label>
                     <Form.Control
                         type="text"
                         value={data.email}
-                        placeholder="Email"
+                        placeholder="ex: john@example.com"
                         onChange={(e) => setData("email", e.target.value)}
                         required
                     />
-                    {errors.email && <div className="ms-2 mt-1 fs-6 fw-light text-danger">{errors.email}</div>}
+                    {errors.email && (
+                        <div className="ms-2 mt-1 fs-6 fw-light text-danger">
+                            {errors.email}
+                        </div>
+                    )}
                 </Form.Group>
-                <Form.Group className="mb-2" controlId="password">
+                <Form.Group
+                    className="mb-2 position-relative"
+                    controlId="password"
+                >
                     <Form.Label>Password</Form.Label>
                     <Form.Control
-                        type="password"
+                        type={showPassword ? "text" : "password"}
                         value={data.password}
-                        placeholder="Password"
+                        placeholder="Masukkan Password"
                         onChange={(e) => setData("password", e.target.value)}
                         required
                     />
-                    {errors.password && <div className="ms-2 mt-1 fs-6 fw-light text-danger">{errors.password}</div>}
+
+                    {/* Ikon mata toggle */}
+                    <i
+                        className={`bi ${
+                            showPassword ? "bi-eye-slash" : "bi-eye"
+                        }`}
+                        onClick={togglePassword}
+                        style={{
+                            position: "absolute",
+                            right: "12px",
+                            top: "43px",
+                            cursor: "pointer",
+                            color: "#6c757d",
+                        }}
+                    ></i>
+
+                    {errors.password && (
+                        <div className="ms-2 mt-1 fs-6 fw-light text-danger">
+                            {errors.password}
+                        </div>
+                    )}
                 </Form.Group>
-                <Form.Group className="mb-2" controlId="checkbox">
+                <Form.Group className="mb-4" controlId="checkbox">
                     <Form.Check
                         type="checkbox"
                         label="Remember me"
