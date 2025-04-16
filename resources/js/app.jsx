dcbf58612c9bb131cceb642bd866ec2330fdb7c3
June <djunee0@gmail.com>
2025-04-16 15:42:46 +0700

    Pelatihan: Bersih2nik dobel

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,22 +1,36 @@
-import './bootstrap';
-import '../css/app.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./bootstrap";
+import "../css/app.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import axios from "axios";
 
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const token = document
+    .querySelector('meta[name="csrf-token"]')
+    ?.getAttribute("content");
+if (token) {
+    axios.defaults.headers.common["X-CSRF-TOKEN"] = token;
+} else {
+    console.error(
+        "CSRF token not found: Pastikan <meta name='csrf-token'> ada di app.blade.php"
+    );
+}
+
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createRoot, hydrateRoot } from "react-dom/client";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.jsx`,
-            import.meta.glob('./Pages/**/*.jsx'),
+            import.meta.glob("./Pages/**/*.jsx")
         ),
     setup({ el, App, props }) {
         if (import.meta.env.SSR) {
@@ -27,6 +41,6 @@ createInertiaApp({
         createRoot(el).render(<App {...props} />);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
