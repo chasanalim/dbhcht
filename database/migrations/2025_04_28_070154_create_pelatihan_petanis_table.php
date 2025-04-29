b56abd6aff76c33fa4b9e81feb31cbeabab6d637
Muzaki <zakizuhdi360@gmail.com>
2025-04-29 10:31:23 +0700

    Pelatihan Petani | Migration Pelatihan Petani

---
+++ database/migrations/2025_04_28_070154_create_pelatihan_petanis_table.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pelatihan_petanis', function (Blueprint $table) {
+            $table->id();
+
+            // A. Data Peserta
+            $table->string('nik', 16)->unique();
+            $table->string('kk', 16);
+            $table->string('jenis_kelamin');
+            $table->string('nama_lengkap');
+            $table->string('no_hp');
+            $table->string('kode_kecamatan');
+            $table->string('kode_kelurahan');
+            $table->string('nama_rw');
+            $table->string('nama_rt');
+            $table->string('alamat');
+            $table->string('alamat_domisili');
+            $table->string('tmp_lhr');
+            $table->date('tgl_lhr');
+            $table->string('pendidikan');
+            $table->boolean('is_disabilitas')->default(false);
+            $table->json('jenis_disabilitas')->nullable();
+
+            // B. Profil Kelompok
+            $table->string('nama_kelompok');
+            $table->string('tahun_berdiri');
+            $table->string('masa_aktif_kelompok');
+            $table->string('bidang_usaha_kelompok');
+            $table->string('kode_kecamatan_kelompok');
+            $table->string('kode_kelurahan_kelompok');
+            $table->string('nama_rw_kelompok');
+            $table->string('nama_rt_kelompok');
+            $table->string('alamat_kelompok');
+
+            // C. Upload Berkas
+            $table->string('file_foto');
+            $table->string('file_ktp');
+            $table->string('file_pengukuhan_penyuluh_swadaya');
+            $table->string('file_rekomendasi_kelompok');
+
+            // D. Kelompok Pelatihan Petani
+            $table->string('kategori');
+            $table->string('jenis_pelatihan_petani');
+
+            // E. Skoring
+            $table->string('alasan');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelatihan_petanis');
+    }
+};
