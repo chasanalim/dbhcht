41b0f31c9ec5499366d496aaffd1a9d1093145c0
June <djunee0@gmail.com>
2025-11-17 10:54:06 +0700

    Pelatihan UMKM | Refactor file URL generation for improved readability

--- app/Http/Controllers/Admin/PelatihanUMKMController.php
+++ app/Http/Controllers/Admin/PelatihanUMKMController.php
@@ -244,27 +244,27 @@ class PelatihanUMKMController extends Controller implements HasMiddleware
                 // Files
                 'files' => [
                     'pasfoto' => [
-                        'url' => asset( $data->file_pasfoto),
+                        'url' => asset($data->file_pasfoto),
                         'verification' => $verifiedDocuments['pasfoto'] ?? null
                     ],
                     'ktp' => [
-                        'url' => asset( $data->file_ktp),
+                        'url' => asset($data->file_ktp),
                         'verification' => $verifiedDocuments['ktp'] ?? null
                     ],
                     'kk' => [
-                        'url' => asset( $data->file_kk),
+                        'url' => asset($data->file_kk),
                         'verification' => $verifiedDocuments['kk'] ?? null
                     ],
                     'surat_pernyataan_tidak_ikut' => [
-                        'url' => asset( $data->file_surat_pernyataan_tidak_ikut_pelatihan),
+                        'url' => asset($data->file_surat_pernyataan_tidak_ikut),
                         'verification' => $verifiedDocuments['surat_pernyataan_tidak_ikut'] ?? null
                     ],
                     'surat_kesanggupan' => [
-                        'url' => asset( $data->file_surat_kesanggupan),
+                        'url' => asset($data->file_surat_kesanggupan),
                         'verification' => $verifiedDocuments['surat_kesanggupan'] ?? null
                     ],
                     'nib' => [
-                        'url' => asset( $data->file_nib),
+                        'url' => asset($data->file_nib),
                         'verification' => $verifiedDocuments['nib'] ?? null
                     ],
                 ],
